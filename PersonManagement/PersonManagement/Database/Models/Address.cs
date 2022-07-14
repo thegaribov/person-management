0fe76f2bc4feda089cd66ecf524ffc8ecffd55a3
zeynalbey <zeynaleg@code.edu.az>
2022-07-14 20:27:53 +0400

    Address model compilated

---
+++ PersonManagement/PersonManagement/Database/Models/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonManagement.Database.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        private static int IdCounter { get; set; }
+
+
+        public Address(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
