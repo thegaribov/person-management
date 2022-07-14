11e4f01fc37156b4cc1d1ae2dcd911abf17d6c97
beta64y <106177539+beta64y@users.noreply.github.com>
2022-07-14 19:23:52 +0300

    Employee Repository

---
+++ PersonManagement/PersonManagement/Database/Repository/EmployeeRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonManagement.Database.Repository
+{
+    internal class EmployeeRepository
+    {
+        public static List<Models.Employee> _employees = new List<Models.Employee>();
+        public void Add(string name , string lastname,string fathername,string fin ,string mail)
+        {
+            Models.Employee employe = new Models.Employee(name,lastname,fathername,fin,mail);
+            _employees.Add(employe);
+
+        }
+        public void Delete(int id)
+        {
+            for(int i = 0; i < _employees.Count; i++)
+            {
+                if (_employees[i].Id == id)
+                {
+                    _employees.RemoveAt(i);
+                }
+            }
+
+
+        }
+    }
+}
