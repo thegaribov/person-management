e7bd0e2895c57886cafe2da1651d6eabedf45147
zeynalbey <zeynaleg@code.edu.az>
2022-07-14 19:58:47 +0400

    added employee class

--- PersonManagement/PersonManagement/Database/Models/Employee.cs
+++ PersonManagement/PersonManagement/Database/Models/Employee.cs
@@ -8,5 +8,27 @@ namespace PersonManagement.Database.Models
 {
     class Employee
     {
+       
+        public int Id { get; set; }
+        private static int IdCounter = 1;
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string Fin { get; set; }
+        public string Email { get; set; }
+
+        public Employee(string name, string lastName, string fatherName, string fin, string email)
+        {
+            Id = IdCounter++;
+
+            Name = name;
+            LastName = lastName;
+            FatherName = fatherName;
+            Fin = fin;
+            Email = email;
+
+
+
+        }
     }
 }
